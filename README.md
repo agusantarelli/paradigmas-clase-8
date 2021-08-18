c473a13d41d042c3ebf258dc6bd1290309653abe
ariel <ariel.ep91@gmail.com>
2021-08-18 18:00:12 -0300

    inital commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Programación reactiva
+
+Este proyecto lo utilizaremos para practicar la programación reactiva.
+
+![](img/reactivity-principles.png)
---
+++ app/src/main/java/paradigms/clase8/App.java
@@ -0,0 +1,8 @@
+package paradigms.clase8;
+
+public class App {
+
+  public static void main(String[] args) {
+    System.out.println("Holaaaa!");
+  }
+}
